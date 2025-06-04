7393b84e6849a1367543eaa5d6f16aa735b698a6
shvveling <95494203+shvveling@users.noreply.github.com>
2025-06-05 03:36:08 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,6 +4,8 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
+from handlers import payment_handler  # BU MUHIM!
+
 
 logging.basicConfig(level=logging.INFO)
 
